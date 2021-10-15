e7e6a78546d70a5dcfd2d6fb40dc53dec93d898b
evlv <evlvdigital@gmail.com>
2021-10-15 16:03:50 +0300

    Create posts-grid componnet for easy reusability.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,19 +2,8 @@
         @include('partials._post-header')
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            @if($posts->count())
-                <x-post-featured-card :post="$posts[0]"/>
-
-                <div class="lg:grid lg:grid-cols-2">
-                @if($posts->count() >1)
-                    @foreach ($posts->skip(1) as $post)
-                        <x-post-card :post="$post" class="bg-red-500"></x-post-card>
-                    @endforeach
-                </div>
-                @endif
-            @else
-                <p>No posts yet, please check back</p>
-            @endif
+            
+            <x-posts-grid :posts="$posts" />
 
         </main>
 
