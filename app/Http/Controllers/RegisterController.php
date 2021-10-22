dd1f58a2edaa7f22d43df46a06b672a039abdb1e
evlv <evlvdigital@gmail.com>
2021-10-22 15:22:49 +0300

    - Upgraded Laravel to v8.65, -Added a registration functionality  with validation rules for registration form inputs.

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'name' => ['required','max:255'],
+            'username' => ['required','max:255','min:3', 'unique:users,username'],
+            'email' => ['required','email','max:255', 'unique:users,email'],
+            'password' => ['required','min:7', 'max:255']
+
+        ]);
+// Ways of to save the user object
+    // First way
+        //     User::create([
+        //         'name' => $attributes['name'],
+        //         'username' => $attributes['username'],
+        //         'email' => $attributes['email'],
+        //         'password' => bcrypt($attributes['password']),
+        // ]);
+    // End first way
+
+    // // Second way
+    //         $attributes['password'] = bcrypt($attributes['password']);
+
+    //         User::create($attributes);
+
+    // // End second way
+
+    // Third way
+            // User::create(request()->validate([
+            //     'name' => ['required','max:255'],
+            //     'username' => ['required','max:255','min:3'],
+            //     'email' => ['required','email','max:255'],
+            //     'password' => ['required','min:7', 'max:255']
+
+            // ]));
+
+    // End third way
+
+
+// End ways to save user object
+// Fourth way would be trhough Eloquent mutator. => in the User model
+        User::create($attributes); // The password is set(mutated in the User model 'SetPasswordAttribute' mutator function)
+        return redirect('/');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var string[]
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -45,5 +41,10 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
     }
 }
