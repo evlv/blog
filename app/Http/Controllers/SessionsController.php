4934576fc252026d57abe6ce79abbffca682b411
evlv <evlvdigital@gmail.com>
2021-10-23 12:00:33 +0300

    Add login functionality with authentications.

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Dotenv\Exception\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // validate the request
+        $attributes = request()->validate([
+            'email' => ['required', 'email', 'exists:users,email'],
+            'password' => ['required']
+        ]);
+
+        // Attempt to authenticate and log in user based on the provided credentials
+        if (! auth()->attempt($attributes)) {
+            // auth failed
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
+        }
+        // session fixation
+        session()->regenerate();
+        // redirect with success message
+        return redirect('/')->with('success', 'Welcome Back!');
+
+    }
+
     public function destroy()
     {
         auth()->logout();
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,36 +1,9 @@
 <x-layout>
     <section class="px-6 py-8">
         <main class="max-w-lg mx-auto mt-10 bg-gray-300  border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl ">Register</h1>
-            <form method="POST" action="/register" class="mt-10">
+            <h1 class="text-center font-bold text-xl ">Login</h1>
+            <form method="POST" action="/login" class="mt-10">
                 @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
-                        Name
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="name"
-                        id="name"
-                        value="{{ old('name')}}"
-                        required
-                    >
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="usename">
-                        Username
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="username"
-                        id="username"
-                        value="{{ old('username')}}"
-                        required
-                    >
-                </div>
                 <div class="mb-6">
                     <Label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
                         Email
@@ -59,9 +32,10 @@
 
                 <div class="mb-6">
                     <button type="submit" class="bg-blue-500 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
+                        Login
                     </button>
                 </div>
+
                 @if ($errors->any())
                     <ul>
                         @foreach ($errors->all() as $error)
